efb81995931b152ef84b8bbff8cabd5a74e4105e
Ubuntu <ubuntu@ip-172-31-39-57.us-east-2.compute.internal>
2019-10-11 21:45:48 +0000

    completed assessment 2

--- js-foundations.js
+++ js-foundations.js
@@ -3,9 +3,30 @@
 
 // 1. Write a function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
 
+var inputNumber = -7
+const divByThree = (inputNumber) => {
+  if (inputNumber % 3 === 0) {
+    return inputNumber + ' is divisible by three'
+  }
+  else if (inputNumber % 3 !== 0){
+    return inputNumber + ' is not divisible by three'
+  }
+  else {
+    return 'Unexpected Input'
+  }
+}
 
+console.log(divByThree(inputNumber))
 
 // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
+
+var helloMe = { firstName: "Ian", lastName: "Thompson", age: 34 ,sex: "male" }
+const outputInfo = (helloMe) => {
+  let sentence = helloMe.firstName + ' ' + helloMe.lastName +' is a ' + helloMe.age + ' year old ' + helloMe.sex
+  return sentence
+}
+
+console.log(outputInfo(helloMe))
 
 
 
@@ -15,15 +36,38 @@ var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "tem
 
 // 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
 
+const everyOther = (randomNouns) => {
+  let newString = ''
+  for (let i=0 ; i<randomNouns.length ; i++){
+    if (i % 2 === 0){
+      newString += randomNouns[i] +' '
+    }
+  }
+  return newString
+}
 
+console.log(everyOther(randomNouns))
 
 // 3b. Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
 
+const firstCap = (item , index , arr) => {
+  let firstLetter = item.charAt(0)
+  let capitalFirst = firstLetter.toUpperCase()
+  let restOfWord = item.slice(1,item.length)
+  let combined = capitalFirst.concat(restOfWord)
+  return combined
+}
+randomNouns.forEach(firstCap)
 
 
 // 3c. STRETCH: Create a function that returns the length of each string from the randomNouns variable. Expected output: [10, 6, 5, 10, 6, 11, 8]
 
+const stringLength = (randomNouns) => {
+  let eachLength = randomNouns.length
+  return eachLength
+}
 
+randomNouns.forEach(stringLength)
 
 
 // -------------------------------------- Consider the variables:
@@ -33,11 +77,19 @@ var testString3 = "sandiego"
 
 // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
 
-
+const alphabetSoup = (testString) => {
+  return testString.split('').sort().join('')
+}
+console.log(alphabetSoup(testString3))
 
 // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
 
-
+const alphabetSoup = (testString1,testString2,testString3) => {
+  let allTogether = testString1+testString2+testString3
+  let alphabetOne = allTogether.split('').sort().join('')
+  return alphabetOne
+}
+console.log(alphabetSoup(testString1,testString2,testString3))
 
 // ------------------------------------- Consider the variables:
 var amounts = [9, 1, 8, 16, 5, 1, 42]
@@ -45,6 +97,30 @@ var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguin
 
 // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
 
+const combiner = (amounts , animals) => {
+  let combinedArray = []
+  for (let i = 0 ; i < amounts.length ; i++){
+    combinedArray[i] = amounts[i] + ' ' + animals[i]
+  }
+  return combinedArray.toString()
+}
+
+console.log(combiner(amounts,animals))
+
 
 
 // 5b. STRETCH: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
+
+const combiner = (amounts , animals) => {
+  let combinedArray = []
+  for (let i = 0 ; i < amounts.length ; i++){
+    if(amounts[i] === 1) {
+      let singleAnimal = animals[i].slice(0,animals[i].length - 1)
+      animals[i] = singleAnimal
+    }
+    combinedArray[i] = amounts[i] + ' ' + animals[i]
+  }
+  return combinedArray.toString()
+}
+
+console.log(combiner(amounts,animals))
--- js-foundations.md
+++ js-foundations.md
@@ -15,66 +15,67 @@ logIt()
 
 1a. Look at this Javascript function and try to predict what the console show.
 
-
+The console.log(text) statement will output undefined to the console.
 
 1b. Test the function. Explain why the function returned what it did.
 
-  Your answer:
+  Your answer: var test is set outside of the function logIt but is not passed into the function as an argument. The console.log statement is before the variable assignment within the function so will not be set to 'inside' until after that line runs.
 
-  Researched answer:
+  Researched answer: Because of scope, not passing the variable text into the function logIt, and order of operations going from top to bottom within the function.
 
 
 2. What is JSON?
 
-  Your answer:
+  Your answer: Method to define objects in javascript
 
-  Researched answer:
+  Researched answer: Stands for JavaScript Object Notation. Is an open-standard file format that uses human-readable text to transmit data objects consisting of attribute–value pairs and array data types. Often used to replace .xml that had been used in the past.
 
 
 3. What does CRUD stand for?
 
-  Your answer:
+  Your answer: Create , Read , Update , Delete
 
-  Researched answer:
+  Researched answer: Create , Read , Update , Delete
 
 
 
 4. What does are the 5 HTTP verbs?
 
-  Your answer:
+  Your answer: Post , Get , Delete, 
 
-  Researched answer:
+  Researched answer: Post , Get , Put , Patch , Delete
 
 
 5. What is a higher-order function?
 
-  Your answer:
+  Your answer: A function that utilizes another function within itself.
 
-  Researched answer:
+  Researched answer: A higher-order function is a function that can take another function as an argument, or that returns a function as a result. 
 
 
 6. STRETCH: What is a closure, what is it good for and how do you recognize one?
 
-  Your answer:
+  Your answer: Complete guess: Something that closes loose ends in code; variables that are defined but not used, etc.
 
-  Researched answer:
+  Researched answer: A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
 
 
 7. STRETCH: What is an API?
 
-  Your answer:
+  Your answer: Programming interface that allows interactions between separate components. Often used to pull information from external sources, like from a database.
 
-  Researched answer:
+  Researched answer: Stands for Application Programming Interface. Is an interface or communication protocol between a client and a server intended to simplify the building of client-side software. It has been described as a “contract” between the client and the server, such that if the client makes a request in a specific format, it will always get a response in a specific format or initiate a defined action.
 
 
 ### Additional Feedback Questions.
 
 1. Do you have a suggestion for a Check In question?
-
+Do you follow any sports? If so any teams that you root for?
 
 
 2. What was your favorite topic this week?
-
+React in general. I have always wondered how websites display a fully featured page with an extremely barebones index.html file. I see that React can be one method to do that.
 
 
 3. What was your "A-ha!" moment this week?
+During the lightbulb app challenge. Figuring out how to use this.state and this.setState
